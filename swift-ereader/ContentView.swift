332ed2c5f540672bf494fecaec7c7ee6ad852164
jasmina <jasmina.jamakovic@gmail.com>
2026-02-15 21:47:33 +0100

    Initial Commit

---
+++ swift-ereader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-ereader
+//
+//  Created by Jasmina on 15/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-ereader/swift_ereaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ereaderApp.swift
+//  swift-ereader
+//
+//  Created by Jasmina on 15/2/26.
+//
+
+import SwiftUI
+
+@main
+struct swift_ereaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
